afabc0a8084de48247cf11c93f80bd40d0bc8d71
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-05-04 23:10:42 +0300

    lab 3 is done

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,6 @@
 import re
 
+# to activate uncomment main_code() at the end of the code file
 
 def read_template(path):
     try:
@@ -17,11 +18,12 @@ def remove_parts(word):
     stop = False
     for char in word:
         if char == "{":
+            str = str + char
             stop = True
-        if stop == False:
-            str = str + char 
         if char == "}":
             stop = False
+        if stop == False:
+            str = str + char 
     return str
 
 def split_and_remove_curly(word):
@@ -44,24 +46,13 @@ def parse_template(str):
         speartor = ""
         word = speartor.join(char_list)
         game_word_list.append(word)
-    return [new_str,game_word_list]
+    return new_str,game_word_list
 
-def replace_part_with_num(word):
-    new_str = ""
-    stop = False
-    counter = 0
-    for char in word:
-        if char == "{":
-            new_str = new_str + char + str(counter)
-            counter += 1
-            stop = True
-        if char == "}":
-            stop = False
-        if stop == False:
-            new_str = new_str + char 
-    return new_str
+def merge(str,tuple):
+    str = str.format(*tuple)
+    return str
 
-def main():
+def main_code():
     welcom_msg = """welcom to new Madlib game
     the game is easy
     Madlib will ask you to enter some some input words depending on the question
@@ -69,21 +60,19 @@ def main():
     print(welcom_msg)
     content = read_template('assets/game_readin_file.txt')
     if content != "no such file":
-        parse_content = parse_template(content)
-        new_str = parse_content[0]
-        game_word_list = parse_content[1]
+        new_str,game_word_list = parse_template(content)
         index = 0
-        # for word in game_word_list:
-        #     print("please insert {} ".format(word))
-        #     replaced_word = input()
-        #     game_word_list[index] = replaced_word
-        #     index += 1
-        content = replace_part_with_num(content)
-        # print(content)
-        print(game_word_list)
-        content = content.format(*game_word_list)
-        # print(content)
+        for word in game_word_list:
+            print("please insert {} ".format(word))
+            replaced_word = input()
+            game_word_list[index] = replaced_word
+            index += 1
+        article = merge(new_str,tuple(game_word_list))
+        writter = open('assets/game_output.txt',"w")
+        writter.write(article)
+        writter.close()
+        print(read_template('assets/game_output.txt'))
 
-main()
+# main_code()
 
 
